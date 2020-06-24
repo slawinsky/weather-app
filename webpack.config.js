511f6cfab178268d8a88fe42b74167eafa82e1f5
Jakub Slawinski <jakubslawinsky@gmail.com>
2020-06-24 23:26:26 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};

30d13a3066103010509d6561aab811a64c34b3db
Jakub Slawinski <jakubslawinsky@gmail.com>
2020-06-26 10:52:19 +0200

    add hidding form in location change

--- src/components/WeatherSearch/WeatherSearch.js
+++ src/components/WeatherSearch/WeatherSearch.js
@@ -3,16 +3,28 @@ import React, { Component } from "react";
 import "./../../styles/WeatherSearch.scss";
 
 class WeatherSearch extends Component {
-  state = {};
+  state = {
+    isActive: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      isActive: !this.state.isActive,
+    });
+  };
 
   render() {
     return (
       <div className="weather-search">
-        <button className="weather-search__btn btn">
+        <button className="weather-search__btn btn" onClick={this.handleClick}>
           <img className="btn__icon" src="src/assets/img/location.svg" />
           <span className="btn__txt">change location</span>
         </button>
-        <form className="weather-search__form form">
+        <form
+          className={`weather-search__form form ${
+            this.state.isActive ? "active" : null
+          }`}
+        >
           <input type="text" className="form__input" />
           <input type="submit" value="ok" className="form__submit" />
         </form>
